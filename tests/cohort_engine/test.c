0b09aa992f78c6c2e8f3aa79b9974b1bfb7d9219
FactorialN <factorialn@169-231-114-208.wireless.ucsb.edu>
2025-04-22 18:53:10 -0700

    added a test program for cohort

---
+++ tests/cohort_engine/test.c
@@ -0,0 +1,14 @@
+// test_cohort.c
+#include <stdint.h>
+
+#define QUEUE_ADDR 0x80001000
+
+int main() {
+    volatile uint64_t *queue = (volatile uint64_t *)QUEUE_ADDR;
+
+    // Write a task to shared memory
+    queue[0] = 42;
+
+    // Loop forever to keep simulation alive
+    while (1);
+}
